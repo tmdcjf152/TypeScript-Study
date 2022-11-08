366187f1305fbcdec79f90493d14b675a86491b3
choiseungchul <tmdcjf152@naver.com>
2022-11-08 22:26:06 +0900

    🌈Add : NarrowingType

---
+++ SC/dist/primitiveType.js
@@ -0,0 +1,30 @@
+/**
+ * 타입스크립트 기본 타입 정리(primitive Types)
+ * 변수명: 타입 이라고 정하면 된다.
+ *
+ * 문자열 : let 이름 :string = 'kim'
+ * 숫자형 : let 나이 :number = 26
+ * 불린형 : let 군필 :boolean = true
+ * 배열 : let 회원목록 :string[] =['kim', 'lee']
+ * 객체 : let 내정보 {name : string} = {name:'kim'}
+ *
+ *> 오늘의 Tip
+ * >변수 생성시 타입을 자동으로 부여해주기 때문에 타입이 따로 지정해줄 필요가 없다.
+ */
+// 1. 이름과 나이 출생지역을 변수로 각각 저장해보자
+var 이름 = '최승철';
+var 나이 = 28;
+var 출생지역 = '서울';
+// 2. 내가 좋아하는 곡과 가수이름을 변수에 객체 자료형으로 담아보자
+var 좋아하는노래 = { 곡명: '가면무도회', 가수명: '비비(BIBI)' };
+// 3. 다음과 같은 자료의 타입을 지정해보록 하자
+// let project = {
+//     member : ['kim', 'park'],
+//     days : 30,
+//     started : true,
+//   }
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true
+};
---
+++ SC/narrowingType.ts
@@ -0,0 +1,48 @@
+/**
+ * Type Narrowing
+ * if문 등으로 타입을 하나로 정해주는 것을 뜻한다.
+
+  function 내함수(x :number | string){
+  if (typeof x === 'number') {
+    return x + 1
+  } 
+  else if (typeof x === 'string') {
+    return x + 1
+  }
+  else {
+    return 0
+  }
+}
+* >✨ 이때 else{}가 없으면 에러가 난다.
+
+ * Type Assertion
+이 변수의 타입을 number로 생각해주세요 라는 의미이다.
+
+function 내함수(x :number | string){ 
+    return (x as number) + 1 
+}
+console.log( 내함수(123) )
+ */
+
+// (숙제1) 숫자여러개를 array 자료에 저장해놨는데
+// 가끔 '4', '5' 이런 식의 문자타입의 숫자가 발견되고 있습니다.
+// 이걸 클리닝해주는 함수가 필요합니다.
+// 클리닝함수( ['1', 2, '3'] ) 이렇게 숫자와 문자가 섞인 array를 입력하면
+// [1,2,3] 이렇게 숫자로 깔끔하게 변환되어 나오는 클리닝함수를 만들어오고 타입지정까지 확실히 해보십시오.
+
+function 클리닝함수(a :(number|string)[]){
+
+  let 클리닝완료 :number[] = [];
+
+  a.forEach((b)=>{
+    if (typeof b === 'string') {
+      클리닝완료.push(parseFloat(b))
+    } else {
+      클리닝완료.push(b)
+    }
+  })
+
+  return 클리닝완료
+}
+
+console.log( 클리닝함수([123,'3']) )
